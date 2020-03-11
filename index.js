f85866345b1c05140f808dd13dc574a02f9f8bc3
차종익 <cha@anytek.com>
2020-03-12 08:59:37 +0900

    babel js, nodemon 적용

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hi");
+import express from "express";
+//const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from Home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
+// // respond with "hello world" when a GET request is made to the homepage
+// app.get('/', function(req, res) {
+//   res.send('hello world');
+// });
