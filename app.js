c52709d5d164f0b4d6447d05172ad747c1183aeb
차종익 <cha@anytek.com>
2020-03-13 08:05:34 +0900

    app 모듈화

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ import bodyparser from "body-parser";
 
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
-
 const handleHome = (req, res) => res.send("Hello from Home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
@@ -27,8 +22,4 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
-// // respond with "hello world" when a GET request is made to the homepage
-// app.get('/', function(req, res) {
-//   res.send('hello world');
-// });
+export default app;
