e0f95151949304333cc9718745da6039eb93c99f
차종익 <cha@anytek.com>
2020-03-13 07:56:36 +0900

    미들웨어 설치

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-import express from "express";
 //const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyparser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -11,12 +16,11 @@ const handleHome = (req, res) => res.send("Hello from Home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-};
-
-app.use(betweenHome); //모든 route에 미들웨어 추가. route 정의 전에 선언해야함.
+app.use(cookieParser());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev")); //모든 route에 미들웨어 추가. route 정의 전에 선언해야함.
 
 //app.get("/", betweenHome, handleHome); //하나의 route에 미들웨어 추가
 app.get("/", handleHome);
