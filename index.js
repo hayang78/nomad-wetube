01852199640257ddc7c75c23b2705aeec0bfba40
차종익 <cha@anytek.com>
2020-03-13 07:31:34 +0900

    미들웨어 테스트

--- index.js
+++ index.js
@@ -11,7 +11,16 @@ const handleHome = (req, res) => res.send("Hello from Home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.use(betweenHome); //모든 route에 미들웨어 추가. route 정의 전에 선언해야함.
+
+//app.get("/", betweenHome, handleHome); //하나의 route에 미들웨어 추가
 app.get("/", handleHome);
+
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
