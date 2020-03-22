9a13a3e0a437da33157f49359178c571a0df6339
차종익 <cha@anytek.com>
2020-03-23 08:09:25 +0900

    Video Model

--- init.js
+++ init.js
@@ -2,6 +2,7 @@ import "./db";
 import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
+import "./models/Video";
 
 const PORT = process.env.PORT || 4000; //env.PORT를 못찾으면 4000 사용
 
