be426bf682396b4070cf97025afb3a79d3eba5c4
차종익 <cha@anytek.com>
2020-03-26 08:09:22 +0900

    webpack 실행시 경고 해결 개발모드와 제품 모드로 분리

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path"); //모던js => import path from "path"
+
+const MODE = process.env.WEBPACK_ENV;
+
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js"); //__dirname: 현재 디레토리 경로, Node.js 전역변수
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
   entry: ENTRY_FILE,
+  mode: MODE,
   output: {
     path: OUTPUT_DIR,
     filename: "[name].[format]"
