eea984ec0db0641a2016c4c41782ae390f0ada25
차종익 <cha@anytek.com>
2020-04-09 07:31:06 +0900

    볼륨 뮤트 구현 및 아이콘 변경

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,6 +1,7 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeButton");
 
 console.log("Hi");
 console.log(videoContainer);
@@ -12,13 +13,26 @@ console.log(videoContainer);
 const handlePlayClick = () => {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
+  }
+};
+
+const handleVolumeClick = () => {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
   }
 };
 
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
 }
 
 if (videoContainer) {
